b6ba8026371f4892afb3792ac117ee70a31e4760
Rodolfo Calzetti <postmaster@rudyz.net>
2014-10-14 14:43:07 +0200

    Release 1.61
    1. Release 1.61

--- _master/apps/corsaro/version.php
+++ _master/apps/corsaro/version.php
@@ -2,7 +2,7 @@
 /****************************************************************************
 * Name:            version.php                                              *
 * Project:         Corsaro                                                  *
-* Version:         1.60                                                     *
+* Version:         1.61                                                     *
 * Description:     Arrows-oriented application                              *
 * Copyright (C):   2014  Rodolfo Calzetti                                   *
 * License GNU GPL: http://www.rudyz.net/apps/corsaro/license.html           *
@@ -10,7 +10,7 @@
 *                  postmaster@rudyz.net                                     *
 ****************************************************************************/
 
-$winz_appversion="1.60";
+$winz_appversion="1.61";
 $copyright_year="2014";
 
 ?>
