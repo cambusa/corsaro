41a1ada2cc8875331667777beef3bb4eff23d002
Rodolfo Calzetti <postmaster@rudyz.net>
2015-03-23 13:29:12 +0100

    Release 1.69.02
    1) ATECO was added.

---
+++ _master/databases/_environs/ryateco.php
@@ -0,0 +1,12 @@
+<?php
+
+$env_maestro="cambusa/ryateco.json";
+$env_monad=true;
+$env_ego=false;
+$env_provider="sqlite";
+$env_strconn=$path_databases."ryauxiliary/ryateco.sqlite";
+$env_user="";
+$env_password="";
+$env_lenid=14;
+
+?>
