2f4578e88f3d9df5fa3865be3ca29c2ed939a106
Rodolfo Calzetti <postmaster@rudyz.net>
2015-04-21 07:29:16 +0200

    Release 1.69.14 beta
    1. Added RYJAX: a XML <=> JSON library.

--- _master/cambusa/rygeneral/jsonx.js
+++ _master/cambusa/rygeneral/jsonx.js
@@ -9,111 +9,102 @@
 * Contact:         faustroll@tiscali.it                                     *
 *                  postmaster@rudyz.net                                     *
 ****************************************************************************/
-function xtoj(xml, tag, doublequote){
-    try{
-        var v=[];
-        var row=0,r,s,exc;
-        var exb=new RegExp(" *$", "gm");
-        var exr=new RegExp("<"+tag+"(>| [^>]*)", "gm");
-        if(doublequote)
-            exc=/(\w+)="([^"]*)"/gm;
-        else
-            exc=/(\w+)='([^']*)'/gm;
-
-        var t=xml.match(exr);
-        for(r in t){
-            v[row]={};
-            while(s=exc.exec(t[r])){
-                v[row][s[1].toUpperCase()]=s[2].replace(exb, "");
-            }
-            row+=1;
-        }
-    }
-    catch(e){
-        if(window.console){console.log(e.message)}
-        var v=[];
-    }
-    return v;
-}
-
-function xxtoj(xml, tag, sub, doublequote){
-    try{
-        var v=[];
-        var row=0,s,t,exc;
-        var exb=new RegExp(" *$", "gm");
-        var exr=new RegExp("<"+tag+"(>| [^>]*)(.+?)</"+tag+">", "gm");
-        if(doublequote)
-            exc=/(\w+)="([^"]*)"/gm;
-        else
-            exc=/(\w+)='([^']*)'/gm;
-        while(t=exr.exec(xml)){
-            v[row]={};
-            while(s=exc.exec(t[1])){
-                v[row][s[1].toUpperCase()]=s[2].replace(exb, "");
-            }
-            v[row]["__DATA__"]=xtoj(t[2], sub, doublequote);
-            row+=1;
-        }
-    }
-    catch(e){
-        if(window.console){console.log(e.message)}
-        var v=[];
-    }
-    return v;
-}
-
-function jtox(json, opt, missing){
-    try{
-        var root="xml";
-        if(opt==missing){opt={}}
-        if(opt.root!=missing){root=opt.root}
-        
-        // CREO UN XML VUOTO
-        var objx=$.parseXML("<"+root+"></"+root+">");
-        
-        // LO CARICO RICORSIVAMENTE CON I DATI DEL DOCUMENTO JSON
-        subjtox(objx, objx.firstChild, json);
-        
-        // RESTITUISCO IL DOCUMENTO XML
-        return objx.firstChild.outerHTML;
-    }
-    catch(e){
-        if(window.console){console.log(e.message)}
-        return "";
-    }
-    function subjtox(objDoc, x, j){
-        for(var i in j){
-            if( isNaN(i) ){
-                if(j[i] instanceof Array){
-                    var e=objDoc.createElement(i);
-                    for(var k in j[i]){
-                        if( isNaN(k) ){
-                            var f=objDoc.createElement("elem");
-                            f.setAttribute(k, j[i][k]);
-                            e.appendChild(f);
-                        }
-                        else if(typeof(j[i])=="object"){
-                            var f=objDoc.createElement("elem");
-                            subjtox(objDoc, f, j[i][k]);
-                            e.appendChild(f);
-                        }
-                    }
-                    x.appendChild(e);
+var RYJAX={
+    xtoj:function(xml, options, missing){
+        try{
+            if(options==missing)options={};
+            if(options.tags==missing)options.tags=[];
+            if(options.doublequote==missing)options.doublequote=true;
+            var v=[];
+            var exb=new RegExp(" *$", "gm");
+            var exc;
+            if(options.doublequote)
+                exc=/(\w+)="([^"]*)"/gm;
+            else
+                exc=/(\w+)='([^']*)'/gm;
+            var exr=[];
+            if(options.tags.length>0){
+                for(var i=0; i<options.tags.length; i++){
+                    if(i<options.tags.length-1)
+                        exr[i]=new RegExp("<"+options.tags[i]+"(>| [^>]*)(.+?)</"+options.tags[i]+">", "gm");
+                    else
+                        exr[i]=new RegExp("<"+options.tags[i]+"(>| [^>]*)", "gm");
                 }
-                else if(typeof(j[i])=="object"){
-                    var e=objDoc.createElement(i);
-                    subjtox(objDoc, e, j[i]);
-                    x.appendChild(e);
+                subxtoj(xml, 0, v);
+            }
+        }
+        catch(e){
+            if(window.console){console.log(e.message)}
+            var v=[];
+        }
+        return v;
+        function subxtoj(subxml, level, subarr){
+            var row=0,s,t;
+            while(t=exr[level].exec(subxml)){
+                subarr[row]={};
+                while(s=exc.exec(t[1])){
+                    subarr[row][s[1].toUpperCase()]=s[2].replace(exb, "");
+                }
+                if(level<options.tags.length-1){
+                    subarr[row]["__DATA__"]=[];
+                    subxtoj(t[2], level+1, subarr[row]["__DATA__"]);
+                }
+                row+=1;
+            }
+        }
+    },
+    jtox:function(json, options, missing){
+        try{
+            if(options==missing){options={}}
+            if(options.root==missing){options.root="xml"}
+            
+            // CREO UN XML VUOTO
+            var objx=$.parseXML("<"+options.root+"></"+options.root+">");
+            
+            // LO CARICO RICORSIVAMENTE CON I DATI DEL DOCUMENTO JSON
+            subjtox(objx, objx.firstChild, json);
+            
+            // RESTITUISCO IL DOCUMENTO XML
+            return objx.firstChild.outerHTML;
+        }
+        catch(e){
+            if(window.console){console.log(e.message)}
+            return "";
+        }
+        function subjtox(objDoc, x, j){
+            for(var i in j){
+                if( isNaN(i) ){
+                    if(j[i] instanceof Array){
+                        var e=objDoc.createElement(i);
+                        for(var k in j[i]){
+                            if( isNaN(k) ){
+                                var f=objDoc.createElement("elem");
+                                f.setAttribute(k, j[i][k]);
+                                e.appendChild(f);
+                            }
+                            else if(typeof(j[i])=="object"){
+                                var f=objDoc.createElement("elem");
+                                subjtox(objDoc, f, j[i][k]);
+                                e.appendChild(f);
+                            }
+                        }
+                        x.appendChild(e);
+                    }
+                    else if(typeof(j[i])=="object"){
+                        var e=objDoc.createElement(i);
+                        subjtox(objDoc, e, j[i]);
+                        x.appendChild(e);
+                    }
+                    else{
+                        x.setAttribute(i, j[i]);
+                    }
                 }
                 else{
-                    x.setAttribute(i, j[i]);
-                }
-            }
-            else{
-                if(typeof(j[i])=="object"){
-                    var e=objDoc.createElement("elem");
-                    subjtox(objDoc, e, j[i]);
-                    x.appendChild(e);
+                    if(typeof(j[i])=="object"){
+                        var e=objDoc.createElement("elem");
+                        subjtox(objDoc, e, j[i]);
+                        x.appendChild(e);
+                    }
                 }
             }
         }
